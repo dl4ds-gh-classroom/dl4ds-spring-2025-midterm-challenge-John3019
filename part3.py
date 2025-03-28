b34d67dc18ca223ce4b77b9a7b297cb3d5190f7c
John Salloum <jsbu2024@bu.edu>
2025-03-28 13:04:52 -0400

    Tuning part 3

--- part3.py
+++ part3.py
@@ -132,9 +132,9 @@ def main():
 
     CONFIG = {
         "model": "MyModel",   # Change name when using a different model
-        "batch_size": 8, # run batch size finder to find optimal batch size
+        "batch_size": 16, # run batch size finder to find optimal batch size
         "learning_rate": 0.001,
-        "epochs": 5,  # Train for longer in a real scenario
+        "epochs": 20,  # Train for longer in a real scenario
         "num_workers": 4, # Adjust based on your system
         "device": "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu",
         "data_dir": "./data",  # Make sure this directory exists
