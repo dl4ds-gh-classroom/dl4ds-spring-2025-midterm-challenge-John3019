13f3cf765994db26ddea70c4f0c5d69849e20a99
John Salloum <jsbu2024@bu.edu>
2025-03-29 17:59:26 -0400

    More epochs

--- part3.py
+++ part3.py
@@ -134,7 +134,7 @@ def main():
         "model": "MyModel",   # Change name when using a different model
         "batch_size": 64, # run batch size finder to find optimal batch size
         "learning_rate": 0.001,
-        "epochs": 25,  # Train for longer in a real scenario
+        "epochs": 100,  # Train for longer in a real scenario
         "num_workers": 4, # Adjust based on your system
         "device": "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu",
         "data_dir": "./data",  # Make sure this directory exists
